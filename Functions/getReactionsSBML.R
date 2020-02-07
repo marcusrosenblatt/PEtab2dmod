9053557dea814ee0a186b39c9c7b0e4fff0ccfd9
marcus.rosenblatt@gmail.com <marcus.rosenblatt@gmail.com>
2020-02-07 15:55:08 +0100

    getReactions preliminary version

--- Functions/getReactionsSBML.R
+++ Functions/getReactionsSBML.R
@@ -1,6 +1,7 @@
 #' Import reactions from SBML. 
 #' 
-#' @description This function imports reactions from SBML.
+#' @description This function imports reactions from SBML. Reactions are written to an eqnlist object. 
+#' Assignment rules for input functions are substituted in the rate column. Time is introduced as an additionel state t.
 #'  
 #' @param model SBML file as .xml
 #'   
@@ -10,26 +11,52 @@
 #'   
 getReactionsSBML <- function(model){
   m = readSBML(model)$getModel()
-  N_reactions <- m$getNumReactions()
+  
   reactions <- NULL
-  for (r in 0:(N_reactions-1)){
+  events <- NULL
+  
+  N_reactions <- m$getNumReactions()
+  for (reaction in 0:(N_reactions-1)){
     Reactantstring <- ""
     Productstring <- ""
-    eq <- m$getModel()$getReaction(r)
-    Reactantnr <- eq$getNumReactants(r)
+    eq <- m$getModel()$getReaction(reaction)
+    Reactantnr <- eq$getNumReactants(reaction)
     if(Reactantnr > 0)Reactantstring <- paste0( eq$getReactant(0)$getStoichiometry(), "*", eq$getReactant(0)$getSpecies())
     if(Reactantnr > 1) for (s in 1:(Reactantnr-1)) {
       Reactantstring <- paste0(Reactantstring, " + ",
                                paste0(eq$getReactant(s)$getStoichiometry(), "*", eq$getReactant(s)$getSpecies()))
     }
-    Productnr <- eq$getNumProducts(r)
+    Productnr <- eq$getNumProducts(reaction)
     if(Productnr > 0) Productstring <- paste0( eq$getProduct(0)$getStoichiometry(), "*", eq$getProduct(0)$getSpecies())
     if(Productnr > 1) for (s in 1:(Productnr-1)) {
       Productstring <- paste0(Productstring, " + ",
                               paste0(eq$getProduct(s)$getStoichiometry(), "*", eq$getProduct(s)$getSpecies()))
     }
-    rate <- sub("pow", "", sub(", ", "**", eq$getKineticLaw()$getFormula()))
+    rate <- sub("pow", "", sub(", ", "**", eq$getKineticLaw()$getFormula())) # to be double checked
+    #rate <- replaceOperation("pow", "**", eq$getKineticLaw()$getFormula())
     reactions <- reactions %>% addReaction(Reactantstring, Productstring, rate) 
   }
-  return(reactions)
+  
+  N_rules <- m$getNumRules()
+  if (N_rules > 0){
+    reactions <- reactions %>% addReaction("", "t", "1")
+    reactions <- reactions %>% addReaction("", "time", "1")
+    for (rule in 0:(N_rules-1)){
+      reactions$rates <- replaceSymbols(m$getRule(rule)$getVariable(),
+                                        paste0("(",m$getRule(rule)$getFormula(), ")"), reactions$rates)  # substitute m$getRule(0)$getVariable() by m$getRule(0)$getFormula()
+    }
+  }
+  
+  
+  for(fun in c("piecewise")){
+    split <- str_split(test, fun)[[1]][2]
+    pos <- which(strsplit(str_split(test, fun)[[1]][2], "")[[1]]==")")[2]
+    event <- paste0(fun, substr(split, 1, pos))
+    events <- c(events, event)
+  }
+  for(i in 1:length(events)){
+    reactions$rates <- str_replace(events[i], paste0("event", i), reactions$rates)
+  }
+  
+  return(list(reactions=reactions, events=events))
 }
