a9af1144499ff0ded08c68526c42576f14105e93
Svenja Kemmer <svenja.kemmer@fdm.uni-freiburg.de>
2020-02-11 14:59:52 +0100

    new function getParametersSBML

---
+++ Functions/getParametersSBML.R
@@ -0,0 +1,42 @@
+#' Import Parameters from PEtab 
+#' 
+#' @description This function imports fixed and fitted parameters from the PEtab parameter file as named vectors.
+#'  
+#' @param parameters PEtab parameter file as .tsv
+#'   
+#' @return constraints and pouter as list of named vectros.
+#'   
+#' @author Marcus Rosenblatt and Svenja Kemmer
+#'   
+getParametersSBML <- function(parameters){
+  mypars <- read.csv(file = parameters, sep = "\t")
+  fixed <- mypars %>% filter(estimate == 0)
+  constraints <- NULL
+  if(nrow(fixed)>0){
+    for(i in 1:length(fixed$parameterScale)) {
+      parscale <- fixed$parameterScale[i]
+      par <- fixed$parameterId[i] %>% as.character()
+      value <- fixed$nominalValue[i]
+      if(parscale == "lin") constraints <- c(constraints, value)
+      if(parscale == "log10") constraints <- c(constraints, log10(value))
+      if(parscale == "log") constraints <- c(constraints, log(value))
+      else paste("This type of parameterScale is not supported.")
+      names(constraints)[i] <- par
+    } 
+  }
+  estimated <- mypars %>% filter(estimate == 1)
+  pouter <- NULL
+  if(nrow(estimated)>0){
+    for(i in 1:length(estimated$parameterScale)) {
+      parscale <- estimated$parameterScale[i]
+      par <- estimated$parameterId[i] %>% as.character()
+      value <- estimated$nominalValue[i]
+      if(parscale == "lin") pouter <- c(pouter, value)
+      if(parscale == "log10") pouter <- c(pouter, log10(value))
+      if(parscale == "log") pouter <- c(pouter, log(value))
+      else paste("This type of parameterScale is not supported.")
+      names(pouter)[i] <- par
+    } 
+  }
+  return(list(constraints=constraints,pouter=pouter))
+}
