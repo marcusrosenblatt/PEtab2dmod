930c731632995e70b8d46601fc76d0b062250ba1
mrosen <marcus.rosenblatt@gmail.com>
2020-02-29 16:37:54 +0100

    Fix: avoid some test functions aborts, add new models to test function

--- testPEtabSBML.R
+++ testPEtabSBML.R
@@ -31,22 +31,32 @@ try_with_time_limit <- function(expr, cpu = Inf, elapsed = Inf){
 }
 
 
-testPEtabSBML <- function(models=c("Boehm_JProteomeRes2014",
-                                   "Fujita_SciSignal2010",
-                                   "Borghans_BiophysChem1997",
-                                   "Elowitz_Nature2000",
-                                   "Sneyd_PNAS2002",
-                                   "Crauste_CellSystems2017",
-                                   "Schwen_PONE2014",
-                                   "Raia_CancerResearch2011",
-                                   "Zheng_PNAS2012"#,
-                                   #"Bachmann_MSB2011"
-                                   #"Lucarelli_CellSystems2018",
+testPEtabSBML <- function(models=c(
+  # "Boehm_JProteomeRes2014",
+  # "Fujita_SciSignal2010",
+  # "Borghans_BiophysChem1997",
+  # "Elowitz_Nature2000",
+  # "Sneyd_PNAS2002",
+  # "Crauste_CellSystems2017",
+  # "Schwen_PONE2014",
+  # "Raia_CancerResearch2011",
+  # "Zheng_PNAS2012",
+  "Beer_MolBioSystems2014",
+  "Brannmark_JBC2010",
+  "Bruno_JExpBio2016",
+  #"Chen_MSB2009",
+  "Fiedler_BMC2016",
+  "Weber_BMC2015",
+  "Swameye_PNAS2003"
+  #"Bachmann_MSB2011"
+  #"Lucarelli_CellSystems2018",
 ), testFit = TRUE, timelimit = 5000){
   cat(green("Start test function...\n"))
+  mywd <- getwd()
   teststarttime <- Sys.time()
   output <- NULL 
   for(model in models){
+    setwd(mywd)
     importtest <- F
     plottest <- F
     bestfit <- NA
@@ -55,41 +65,55 @@ testPEtabSBML <- function(models=c("Boehm_JProteomeRes2014",
       {test <- try(importPEtabSBML(model, compile=T), silent = T)
       if(inherits(test, "try-error")) "import error" else test}
       , timelimit)
-    if(fgh=="import error") cat(yellow("Import error or time limit exceeded for", model, "\n")) else {
-        testobj <- obj(pouter)
-        if(is.numeric(testobj$value)) cat(green("Calculation of objective function successful.\n")) else cat(red("Warning: obj(pouter) is not numeric.\n"))
-        if(testFit){
-          fitstarttime <- Sys.time()
-          myframe <- fitModelPEtabSBML(nrfits=20)
-          fitendtime <- Sys.time()
-          if(is.parframe(myframe) & !is.null(myframe))
-            if(is.numeric(obj(myframe[1,])$value)){
-              cat(green("Fit test successful.\n"))
-              bestfit <- obj(myframe[1,])$value
-            } else cat(red("Warning: obj(myframe) is not numeric.\n"))
-          else cat(red("Warning: Fit test not successful..\n"))
-          mytimediff <- as.numeric(difftime(fitendtime, fitstarttime, unit="secs"))
-          if(mytimediff > 3600) cat(green(paste0("Fitting done in ",as.character(format(as.numeric(difftime(fitendtime, fitstarttime, unit="hours")), digits=3)), " hours.\n"))) else
-            if(mytimediff > 60) cat(green(paste0("Fitting done in ",as.character(format(as.numeric(difftime(fitendtime, fitstarttime, unit="mins")), digits=3)), " minutes.\n"))) else
-              cat(green(paste0("Fitting done in ",as.character(format(as.numeric(difftime(fitendtime, fitstarttime, unit="secs")), digits=3)), " seconds.\n")))
-          
-        }
-        pdf(file = paste0("Test/",model,"_plotAll.pdf"))
-        plotPEtabSBML()
-        dev.off()
-        pdf(file = paste0("Test/",model,"_plotTargetsObserved.pdf"))
-        plotPEtabSBML(name%in%names(observables))
-        dev.off()
-        pdf(file = paste0("Test/",model,"_plotConditionsObserved.pdf"))
-        plotPEtabSBML(condition%in%names(mydata))
-        dev.off()
+    if(fgh=="import error"){
+        cat(yellow("Import error or time limit exceeded for", model, "\n\n\n"))
+      output <- rbind(output, data.frame(modelname = model, import = importtest, 
+                                         fitting_time=NA, plot=plottest, objective_value = NA, bestfit=NA, difference=NA))
+    } else {
         importtest <- T
-        plottest <- T
-        cat(green("Import and plot test for ",fgh, " successful!\n\n\n"))
+        testobj <- try(obj(pouter))
+        if(inherits(testobj, "try-error")){
+          cat(red("Warning: Error in calculation of objective function.\n"))
+          output <- rbind(output, data.frame(modelname = model, import = importtest, 
+                                             fitting_time=NA, plot=plottest, objective_value = NA, bestfit=NA, difference=NA))
+        } else {
+          if(is.numeric(testobj$value))
+            cat(green("Calculation of objective function successful.\n")) 
+          else cat(red("Warning: obj(pouter) is not numeric.\n"))
+          if(testFit){
+            fitstarttime <- Sys.time()
+            myframe <- fitModelPEtabSBML(nrfits=20)
+            fitendtime <- Sys.time()
+            if(is.parframe(myframe) & !is.null(myframe))
+              if(is.numeric(obj(myframe[1,])$value)){
+                cat(green("Fit test successful.\n"))
+                bestfit <- obj(myframe[1,])$value
+              } else cat(red("Warning: obj(myframe) is not numeric.\n"))
+            else cat(red("Warning: Fit test not successful..\n"))
+            mytimediff <- as.numeric(difftime(fitendtime, fitstarttime, unit="secs"))
+            if(mytimediff > 3600) cat(green(paste0("Fitting done in ",as.character(format(as.numeric(difftime(fitendtime, fitstarttime, unit="hours")), digits=3)), " hours.\n"))) else
+              if(mytimediff > 60) cat(green(paste0("Fitting done in ",as.character(format(as.numeric(difftime(fitendtime, fitstarttime, unit="mins")), digits=3)), " minutes.\n"))) else
+                cat(green(paste0("Fitting done in ",as.character(format(as.numeric(difftime(fitendtime, fitstarttime, unit="secs")), digits=3)), " seconds.\n")))
+            
+          }
+          pdf(file = paste0("Test/",model,"_plotAll.pdf"))
+          plotPEtabSBML()
+          dev.off()
+          pdf(file = paste0("Test/",model,"_plotTargetsObserved.pdf"))
+          plotPEtabSBML(name%in%names(observables))
+          dev.off()
+          pdf(file = paste0("Test/",model,"_plotConditionsObserved.pdf"))
+          plotPEtabSBML(condition%in%names(mydata))
+          dev.off()
+          plottest <- T
+          cat(green("Import and plot test for ",fgh, " successful!\n\n\n"))
+          
+          output <- rbind(output, data.frame(modelname = model, import = importtest, 
+                                             fitting_time=format(as.numeric(difftime(fitendtime, fitstarttime, unit="mins")), digits=3),
+                                             plot=plottest, objective_value = testobj$value, bestfit=bestfit, difference=bestfit-testobj$value))
+        }
+          
     }
-    output <- rbind(output, data.frame(modelname = model, import = importtest, 
-                                       fitting_time=format(as.numeric(difftime(fitendtime, fitstarttime, unit="mins")), digits=3),
-                                       plot=plottest, objective_value = testobj$value, bestfit=bestfit, difference=bestfit-testobj$value))
   }
   testendtime <- Sys.time()
   mytimediff <- as.numeric(difftime(testendtime, teststarttime, unit="secs"))
